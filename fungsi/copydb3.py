38f138b2b6aa5494534369f72d0b64b907ea2e50
merdekabayu <merdekabayu06@gmail.com>
2023-08-02 23:19:41 +0900

    copydb

---
+++ fungsi/copydb3.py
@@ -0,0 +1,20 @@
+from datetime import datetime,timedelta
+import os
+from os import path,makedirs
+
+today = datetime.now()
+yest = today - timedelta(1)
+tgl = yest.strftime('%Y%m%d')
+date = yest.strftime('%Y-%m-%d')
+f_thn = yest.strftime('%Y')
+f_bln = yest.strftime('%#m')
+dest_folder = f_thn+'/'+f_bln
+
+des = '~/sigempa_v2/sipetir/fungsi/data/DB3'+'/'+dest_folder
+if path.exists(des):
+    print('folder '+des+' ready!')
+else:
+    makedirs(des)
+    print('folder created!')
+
+os.system('cp -r /home/ftpternate/DB3/'+tgl+'.db3 '+des)
