b13910e7ea1604f9b5094e641b1a16c730b3fb60
merdekabayu <merdekabayu06@gmail.com>
2023-08-02 23:35:36 +0900

    b

--- fungsi/copydb3.py
+++ fungsi/copydb3.py
@@ -8,7 +8,7 @@ tgl = yest.strftime('%Y%m%d')
 date = yest.strftime('%Y-%m-%d')
 f_thn = yest.strftime('%Y')
 f_bln = yest.strftime('%-m')
-dest_folder = f_thn+'/'+f_bln+'/'
+dest_folder = f_thn+'/'+f_bln
 
 des = '~/sigempa_v2/sipetir/fungsi/data/DB3'+'/'+dest_folder
 if path.exists(des):
